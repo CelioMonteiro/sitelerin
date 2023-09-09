9829cc8971bb9b9cf632b64fb5e48ee503cc0833
Celio Monteiro <celio.monteiro.silva@gmail.com>
2023-09-08 22:10:54 -0300

    update mac

---
+++ php/email.php
@@ -0,0 +1,9 @@
+<?php
+$to = $_POST['email'];
+$subject = $_POST['assunto'];
+$txt = $_POST['mensagem'];
+$headers = "From: contato@lerin.com.br" . "\r\n" .
+"CC: celio.monteiro.silva@gmail.com";
+
+mail($to,$subject,$txt,$headers);
+?>
