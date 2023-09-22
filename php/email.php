020ad1a913b7f7be220e78df032564daad1ded33
Celio Monteiro <celio.monteiro.silva@gmail.com>
2023-09-22 10:40:48 -0300

    update

--- php/email.php
+++ php/email.php
@@ -1,18 +1,20 @@
 <?php
-$to               = 'contato@lerin.com.br';
-$subject          = $_POST['assunto'];
-$mensagemHTML     = 'nome: '.$_POST['nome']."\r\n"."email: ".$_POST['email']."\r\n".$_POST['mensagem'];
-
-$emailsender = "contato@lerin.com.br";
-$headers = "Content-type: text/html; charset=UTF-8\r\n";
-$headers .= "Return-Path: " . $emailsender . "\r\n"; 
-
-if(!mail('celio.monteiro.silva@gmail.com', $subject, $mensagemHTML, $headers ,"-r".$emailsender)){ // Se for Postfix
-    $headers .= "Return-Path: " . $emailsender . $quebra_linha; // Se "não for Postfix"
-    mail('celio.monteiro.silva@gmail.com', $subject, $mensagemHTML, $headers );
+ //$to      = $dados['email'];
+ $to = 'celio.monteiro.silva@gmail.com';
+ $subject = 'Lerin - Desenvolvimento WEB';
+ $mensagemHTML = 'teste';
+ $emailsender = "contato@lerin.com.br";
+ $headers = "Content-type: text/html; charset=UTF-8\r\n";
+ $headers .= "Return-Path: " . $emailsender . "\r\n"; 
+ 
+ if(!mail($to, $subject, $mensagemHTML, $headers ,"-r".$emailsender)){ // Se for Postfix
+     $headers .= "Return-Path: " . $emailsender . $quebra_linha; // Se "não for Postfix"
+     mail("celio.monteiro.silva@gmail.com", $subject, $mensagemHTML, $headers );
     
-
-}
+ 
+ }
+  
+ echo $to.' - Enviado com sucesso'. "\r\n"; 
 
 echo json_encode('enviado com sucesso!', JSON_PRETTY_PRINT); 
 ?>
